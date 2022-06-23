c56815a5464ee5bfff4c863c5dc8911a72f9e3f0
youchew14 <66003423+youchew14@users.noreply.github.com>
2022-06-23 11:34:57 +0900

    [Bronze V] Title: 새싹, Time: 56 ms, Memory: 5300 KB -BaekjoonHub

--- 백준/Bronze/25083. 새싹/새싹.cs
+++ 백준/Bronze/25083. 새싹/새싹.cs
@@ -1,6 +1,6 @@
-            Console.WriteLine("         ,r'\"7");
-            Console.WriteLine("r`-_   ,'  ,/");
-            Console.WriteLine(" \\. \". L_r'");
-            Console.WriteLine("   `~\\/");
-            Console.WriteLine("      |");
-            Console.WriteLine("      |");
+Console.WriteLine("         ,r'\"7");
+Console.WriteLine("r`-_   ,'  ,/");
+Console.WriteLine(" \\. \". L_r'");
+Console.WriteLine("   `~\\/");
+Console.WriteLine("      |");
+Console.WriteLine("      |");
