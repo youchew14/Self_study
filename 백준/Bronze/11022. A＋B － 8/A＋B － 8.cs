6012792a48aeaefe896b24426670820733d2c8bb
youchew14 <66003423+youchew14@users.noreply.github.com>
2022-06-24 11:11:08 +0900

    [Bronze III] Title: A+B - 8, Time: 80 ms, Memory: 5800 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cs
@@ -0,0 +1,15 @@
+using System.Text;
+
+StringBuilder num = new StringBuilder();
+
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 1; i < n+1; i++)
+{
+    string[] user = Console.ReadLine().Split();
+    int a = int.Parse(user[0]);
+    int b = int.Parse(user[1]);
+
+    num.Append("Case #" + i + ": " + a + " + " + b + " = " + (a + b) + "\n"); // 문자열로 저장된다.
+}
+Console.WriteLine(num.ToString());
