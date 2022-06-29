fb53fdd7c8b39c918c7ecc1d51ec4db1b51a00ea
youchew14 <66003423+youchew14@users.noreply.github.com>
2022-06-29 12:54:48 +0900

    [Bronze II] Title: 손익분기점, Time: 1384 ms, Memory: 5800 KB -BaekjoonHub

---
+++ 백준/Bronze/1712. 손익분기점/손익분기점.cs
@@ -0,0 +1,25 @@
+string[] user = Console.ReadLine().Split();
+long a = int.Parse(user[0]);
+long b = int.Parse(user[1]);
+long c = int.Parse(user[2]);
+
+int n = 0;
+bool point = true;
+
+if (b >= c)
+{
+    point = false;
+    n = -1;
+}
+while (point)
+{
+    n++;
+    // 총 수입 > 총 비용 = 손익분기점 발생
+    if (c * n > a + (b * n))
+    {
+        //Console.WriteLine("총 수입 : " + (c * n));
+        //Console.WriteLine("총 비용 : " + (a + (b * n)));
+        break;
+    }
+}
+Console.WriteLine(n);
