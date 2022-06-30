0ec39ea4209b76a982103c818cfc03cd132e5de0
youchew14 <66003423+youchew14@users.noreply.github.com>
2022-06-30 11:16:30 +0900

    [Bronze V] Title: 아!, Time: 60 ms, Memory: 5348 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.cs
@@ -0,0 +1,11 @@
+string a = Console.ReadLine(); // 제시하는 길이
+string b = Console.ReadLine(); // 제시된 길이
+
+if (a.Length < b.Length)
+{
+    Console.WriteLine("no");
+}
+else
+{
+    Console.WriteLine("go");
+}
