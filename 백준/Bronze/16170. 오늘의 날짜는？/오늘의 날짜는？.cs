424c76b64673e193d06c59b5161cc3695df46c63
youchew14 <66003423+youchew14@users.noreply.github.com>
2022-07-04 14:08:37 +0900

    [Bronze V] Title: 오늘의 날짜는?, Time: 64 ms, Memory: 5528 KB -BaekjoonHub

---
+++ 백준/Bronze/16170. 오늘의 날짜는？/오늘의 날짜는？.cs
@@ -0,0 +1,7 @@
+string Datey = DateTime.Now.ToString("yyyy");
+string Datem = DateTime.Now.ToString("MM");
+string Dated = DateTime.Now.ToString("dd");
+
+Console.WriteLine(Datey);
+Console.WriteLine(Datem);
+Console.WriteLine(Dated);
