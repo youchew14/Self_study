bd2d267fcc79573f0bb3183dcbed04459d12e5b5
youchew14 <66003423+youchew14@users.noreply.github.com>
2022-07-04 11:15:47 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 60 ms, Memory: 5464 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cs
@@ -0,0 +1,8 @@
+string[] user = Console.ReadLine().Split(' ');
+int[] num = Array.ConvertAll(user, int.Parse);
+
+for (int i = 0; i < num[0]; i++)
+{
+    string[] bus = Console.ReadLine().Split();
+}
+Console.WriteLine("비와이");
