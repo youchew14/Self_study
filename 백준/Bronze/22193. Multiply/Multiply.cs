0edcae0f77bf455bd94a0272c62c128b51f539f6
youchew14 <66003423+youchew14@users.noreply.github.com>
2022-07-04 16:32:44 +0900

    [Bronze V] Title: Multiply, Time: 228 ms, Memory: 8448 KB -BaekjoonHub

---
+++ 백준/Bronze/22193. Multiply/Multiply.cs
@@ -0,0 +1,8 @@
+using System.Numerics;
+
+string[] user = Console.ReadLine().Split(' ');
+
+BigInteger n = BigInteger.Parse(Console.ReadLine());
+BigInteger m = BigInteger.Parse(Console.ReadLine());
+
+Console.WriteLine(n * m);
