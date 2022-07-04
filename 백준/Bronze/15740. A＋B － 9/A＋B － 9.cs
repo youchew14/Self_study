045d07ac37db80c2060905c0d17177716d515722
youchew14 <66003423+youchew14@users.noreply.github.com>
2022-07-04 13:51:21 +0900

    [Bronze V] Title: A+B - 9, Time: 80 ms, Memory: 5880 KB -BaekjoonHub

---
+++ 백준/Bronze/15740. A＋B － 9/A＋B － 9.cs
@@ -0,0 +1,7 @@
+using System.Numerics;
+
+string[] user = Console.ReadLine().Split(' ');
+BigInteger a = BigInteger.Parse(user[0]);
+BigInteger b = BigInteger.Parse(user[1]);
+
+Console.WriteLine(a + b);
