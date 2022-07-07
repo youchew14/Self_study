fdc99584194148f5eab4f36604c9c104b0ad9a69
youchew14 <66003423+youchew14@users.noreply.github.com>
2022-07-07 15:21:59 +0900

    [Bronze IV] Title: ISBN, Time: 60 ms, Memory: 5468 KB -BaekjoonHub

---
+++ 백준/Bronze/6810. ISBN/ISBN.cs
@@ -0,0 +1,5 @@
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+Console.WriteLine("The 1-3-sum is {0}", 91 + a + (3 * b) + c);
