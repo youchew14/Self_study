09c4e1e0d970b04dd3afcdb137c89e1042962810
youchew14 <66003423+youchew14@users.noreply.github.com>
2022-07-11 00:25:54 +0900

    [Bronze IV] Title: Archivist, Time: 72 ms, Memory: 5444 KB -BaekjoonHub

---
+++ 백준/Bronze/20232. Archivist/Archivist.cs
@@ -0,0 +1,5 @@
+int user = int.Parse(Console.ReadLine());
+
+string[] win = { "ITMO", "SPbSU", "SPbSU", "ITMO", "ITMO", "SPbSU", "ITMO", "ITMO", "ITMO", "ITMO", "ITMO", "PetrSU, ITMO", "SPbSU", "SPbSU", "ITMO", "ITMO", "ITMO", "ITMO", "SPbSU", "ITMO",  "ITMO", "ITMO", "ITMO", "SPbSU", "ITMO"};
+
+Console.WriteLine(win[user - 1995]);
