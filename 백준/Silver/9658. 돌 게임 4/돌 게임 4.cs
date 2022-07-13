8ed5335de18794a4bf2a1ad28759da5a2a438906
youchew14 <66003423+youchew14@users.noreply.github.com>
2022-07-13 11:27:07 +0900

    [Silver II] Title: 돌 게임 4, Time: 76 ms, Memory: 5496 KB -BaekjoonHub

---
+++ 백준/Silver/9658. 돌 게임 4/돌 게임 4.cs
@@ -0,0 +1,30 @@
+int n = int.Parse(Console.ReadLine());
+int[] arr = new int[1001];
+
+arr[1] = 0;
+arr[2] = 1;
+arr[3] = 0;
+arr[4] = 1;
+
+if (n >= 5)
+{
+    for (int i = 5; i < n + 1; i++)
+    {
+        if (Math.Min(arr[i-1], Math.Min(arr[i-3], arr[i-4])) == 1)
+        {
+            arr[i] = 0;
+        }
+        else
+        {
+            arr[i] = 1;
+        }
+    }
+}
+if (arr[n] == 0)
+{
+    Console.WriteLine("CY");
+}
+else
+{
+    Console.WriteLine("SK");
+}
