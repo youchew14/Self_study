6c4a65e2cc0af15a441ce201a5032ca032d0c7e3
youchew14 <66003423+youchew14@users.noreply.github.com>
2022-07-14 09:50:20 +0900

    [Bronze III] Title: 윷놀이, Time: 64 ms, Memory: 5480 KB -BaekjoonHub

---
+++ 백준/Bronze/2490. 윷놀이/윷놀이.cs
@@ -0,0 +1,19 @@
+for (int k = 0; k < 3; k++)
+{
+    string[] s = Console.ReadLine().Split();
+    if (s == null || s.Length >= 5) break;
+
+    int[] n = Array.ConvertAll(s, int.Parse);
+
+    int count = 0;
+
+    for (int j = 0; j < 4; j++)
+    {
+        if (n[j] == 1)
+        {
+            count++;
+        }
+    }
+    string[] yut = {"D", "C", "B", "A", "E"};
+    Console.WriteLine(yut[count]);
+}
