0838f8b8e420d7efcc9566940e5743eed0ffdf8e
youchew14 <66003423+youchew14@users.noreply.github.com>
2022-07-26 11:00:33 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2476 ms, Memory: 20044 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cs
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cs
@@ -3,14 +3,14 @@ StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput
 int n = int.Parse(Console.ReadLine());
 int[] nn = new int[10001];
 
-for (int i = 0; i < n; i++)
+while(n-- > 0)
 {
     int ll = int.Parse(Console.ReadLine());
     nn[ll]++;
 }
 for (int i = 0; i < nn.Length; i++)
 {
-    for(int j = 0; j < nn[i]; j++)
+    while(nn[i]-- > 0)
     {
         sw.WriteLine(i);
     }
